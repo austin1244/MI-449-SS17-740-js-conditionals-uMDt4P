98de06382f86bcc6f155ccc06a5d6df41a1d6f2b
Austin1244 <rixausti@msu.edu>
2017-02-06 08:00:49 -0500

    upddated script

--- script.js
+++ script.js
@@ -6,22 +6,36 @@ if(name.length > 15){
     name = name.substring(0,1).trim().toUpperCase();
   }
 }
+
 var option = ""
 while(option != "left" || option != "right"){
   option = window.prompt(name+"!!! There is a car comming at us! should we go right or left???");
   option = option.trim().toLowerCase();
   if(option === "left"){
     alert("you dove into a another car and died :(");
+    exit();
   }
-  else if(option === "right"){
-    option = "";
-    while(option != "left" || option != "right"){
-     option = window.prompt("you dodged the car, but there is another car comming!!! right or left?");
-    }
+  else if(option === 'right'){
+    break;
   }
   else{
-    alert(name+"? That is not an option"); 
+    alert(name+"? That is not an option");
   }
-
 }
 
+option = "";
+while(option != "left" || option != "right"){
+  option = window.prompt("you dodged the car, but there is another car comming!!! right or left?");
+  option = option.trim().toLowerCase();
+  if(option == "right"){
+   alert("you dove into a another car and died :(");
+   break;
+  }
+  else if(option === "left"){
+   alert("You dodged the car and survived. Congratz!!!");
+   break;
+  }
+  else{
+   alert(name+"? That is not an option");
+ }
+}
